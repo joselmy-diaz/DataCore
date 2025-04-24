c3694c891120e2ecc0f7e56522e641a6a3dfe55e
joselmy-diaz <joselmydiaz31@gmail.com>
2025-04-24 20:56:57 +0000

    version mas otimisada

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/joselmy/Desktop/Kintsugi/kit/DataCore
-# Build directory: /home/joselmy/Desktop/Kintsugi/kit/DataCore/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("test")
